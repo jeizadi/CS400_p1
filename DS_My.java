a9177d43d7c751799a7e65ccf7cf7b8a0fe950a8
Jenna Eizadi <jenna@Eizadis-MacBook-2.local>
2019-02-07 13:25:24 -0600

    final commit

--- DS_My.java
+++ DS_My.java
@@ -2,7 +2,7 @@
 public class DS_My implements DataStructureADT {
 
 	private KeyPair head; //reference to the head element in the list
-	int size;
+	int size; //holds value to the size of the list 
 	
     public DS_My() {
     	head = null;
@@ -10,37 +10,37 @@ public class DS_My implements DataStructureADT {
     }
 
     @Override
-    public void insert(Comparable k, Object v) {
+    public void insert(Comparable k, Object v) { //method to insert a new keypair into the list
     	if(k == null) throw new IllegalArgumentException("null key"); //check if key is null
-    	if(this.contains(k)) throw new RuntimeException("duplicate key");
-    	KeyPair current = new KeyPair(k, v);
+    	if(this.contains(k)) throw new RuntimeException("duplicate key"); //checks to make sure there isn't already a keypair with the inputed key
+    	KeyPair current = new KeyPair(k, v); //creates a new keypair with the key and value passed in
     	current.prev = null;
-    	current.next = head;
+    	current.next = head; //adds this new keypair to the front of the list by saying that what comes after it is the head
     	if(head != null) {
-    		head.prev = current;
+    		head.prev = current; //changes the head's previous pointer to the new keypair 
     	}
-    	head = current;
-    	size++;
+    	head = current; //mark the new keypair as the head because it is now at the front of the list
+    	size++; //add one to the size
     }
 
     @Override
-    public boolean remove(Comparable k) {
-    	if(k == null) throw new IllegalArgumentException("null key");
+    public boolean remove(Comparable k) { //method to remove a keypair with the specified key from the list
+    	if(k == null) throw new IllegalArgumentException("null key"); //check if key is null
     	if(this.contains(k) == false) return false; //checks whether the key is in the list and returns false if it isn't
-    	KeyPair current = head;
-    	if(head == null) return false;
-    	while(current != null) {
-    		if(current.key.compareTo(k) == 0 ) {
-    			if(head == current) {
-    				head = current.next;
+    	KeyPair current = head; //creates new variable pointing to the head
+    	if(head == null) return false; //if list is empty remove cannot be performed
+    	while(current != null) { //iterate through the list
+    		if(current.key.compareTo(k) == 0 ) { //if the correct key is found
+    			if(head == current) { //if key is in keypair at the head
+    				head = current.next; //change reference to head to next keypair
     			}
-    			if(current.next != null) {
-    				current.next.prev = current.prev;
+    			if(current.next != null) { //if the next keypair is not null
+    				current.next.prev = current.prev; //that keypair's prev pointer will be to the keypair before the one to be removed
     			}
-    			if(current.prev != null) {
-    				current.prev.next = current.next;
+    			if(current.prev != null) { //if the previous keypair is not null
+    				current.prev.next = current.next; //that keypair's next pointer will be to the keypair after the one to be removed 
     			}
-    			size--;
+    			size--; //remove one from the size
     			return true;
     		}
     		current = current.next;
@@ -49,65 +49,55 @@ public class DS_My implements DataStructureADT {
     }
 
     @Override
-    public boolean contains(Comparable k) {
-    	//iterate through the list checking if the keys match with the one given  
-    	//return true if one does otherwise return false
-    	KeyPair current = head;
+    public boolean contains(Comparable k) { //iterate through the list checking if the keys match with the one given  
+    	KeyPair current = head; //creates new variable pointing to the head
     	if(k == null) return false; //check to see if the key given is null
     	while(current != null) { //iterate through list 
     		if(current.key.compareTo(k) == 0) { //check keys to see if they are equal to k
-    			return true;
+    			return true; //if key is found in the list return true
     		}
     		current = current.next; 
     	}
-        return false;
+        return false; //otherwise return false
     }
 
     @Override
-    public Object get(Comparable k) {
-    	KeyPair current = head;
+    public Object get(Comparable k) { //return value associated with the key passed in 
+    	KeyPair current = head; //creates new variable pointing to the head
     	if(k == null) throw new IllegalArgumentException("null key"); //check to see if the key given is null
-    	while(current.next != null) {
+    	while(current.next != null) { //iterate through list
     		if(current.key.compareTo(k) == 0) { //check keys to see if they are equal to k
-    			return current.value;
+    			return current.value; //if keypair with associated key is in list, return the value
     		}
     		current = current.next; 
     	}
-    	if(current.key.compareTo(k) == 0) return current.value; //check end cases
-        return null;
+    	if(current.key.compareTo(k) == 0) return current.value; //checks end cases
+        return null; //if key not found return null
     }
 
     @Override
-    public int size() {
+    public int size() { //returns the size of the list
     	return size;
     }
     
-    //private class KeyPair that holds key and value KeyPairs as well as reference to next
-    class KeyPair { 
-    	private Comparable key;
-    	private Object value;
-    	private KeyPair next;
-    	private KeyPair prev;
+    class KeyPair { //private class KeyPair that holds key and value KeyPairs as well as reference to next
+    	private Comparable key; //key for the KeyPair	
+    	private Object value; //value of the KeyPair
+    	private KeyPair next; //reference to the next KeyPair in the list
+    	private KeyPair prev; //reference to the previous KeyPair in the list
     	
-    	private KeyPair(Comparable key, Object value, KeyPair next, KeyPair previous) {
+    	private KeyPair(Comparable key, Object value, KeyPair next, KeyPair previous) { //create a KeyPair object is prev and next are known
     		this.key = key;
     		this.value = value;
     		this.next = next;
     		this.prev = previous;
     	}
     	
-    	private KeyPair(Comparable key, Object value) {
+    	private KeyPair(Comparable key, Object value) { //create a KeyPair object with unknown prev and next values
     		this.key = key;
     		this.value = value;
     	}
     	
-    	private void setNext(KeyPair n) {
-    		next = n;
-    	}
-    	
-    	private void setPrev(KeyPair p) {
-    		prev = p;
-    	}
     	
     }
 
--- DataStructureADTTest.java
+++ DataStructureADTTest.java
@@ -1,3 +1,11 @@
+/*
+ *Assignment: Project 1 
+ *Author: Jenna Eizadi
+ *CS 400 Lecture 001
+ *Email: eizadi@wisc.edu
+ *Due Date: 02/07/2019 
+ *Source Credits: http://stackoverflow.com for helping me resolve a few issues I was having with my remove method
+ */
 import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.AfterAll;
@@ -31,51 +39,48 @@ abstract class DataStructureADTTest<T extends DataStructureADT<String,String>> {
 	}
 
 	
+	//checks to make sure the data structure is empty when it is supposed to be
 	@Test
 	void test00_empty_ds_size() {
-		if (dataStructureInstance.size() != 0)
+		if (dataStructureInstance.size() != 0) //check to make sure that the size of an empty list is 0
 			fail("data structure should be empty, with size=0, but size="+dataStructureInstance.size());
 	}
 	
-	// test01_after_insert_one_size_is_one
 	//insert one key value pair into the data structure and then confirm that size() is 1
 	@Test
 	void test01_after_insert_one_size_is_one() {
-		dataStructureInstance.insert(new String ("key"), new String ("value"));
-		if(dataStructureInstance.size() != 1)
+		dataStructureInstance.insert(new String ("key"), new String ("value")); //insert a key value pair
+		if(dataStructureInstance.size() != 1) //check to see if the size reflects this insert
 			fail("data structure should have one key, value pair, with a size=1, but size="+dataStructureInstance.size());
 	}
 	
-	// test02_after_insert_one_remove_one_size_is_0
 	// insert one key,value pair and remove it, then confirm size is 0
 	@Test
 	void test02_after_insert_one_remove_one_size_is_0() {
-		dataStructureInstance.insert(new String("key"), new String("value"));
-		dataStructureInstance.remove("key");
-		if (dataStructureInstance.size() != 0)
+		dataStructureInstance.insert(new String("key"), new String("value")); //insert a value
+		dataStructureInstance.remove("key"); //remove that value
+		if (dataStructureInstance.size() != 0) //see if the removal was successful by checking the size
 			fail("data structure should be empty, with size=0, but size="+dataStructureInstance.size());
 	}
 	
-	// test03_duplicate_exception_is_thrown
 	//insert a few key,value pairs such that one of them has the same key as an earlier one.  Confirm that a RuntimeException is thrown
 	@Test
 	void test03_duplicate_exception_is_thrown() {
 		try {
-			dataStructureInstance.insert(new String("key"), new String("value"));
-			dataStructureInstance.insert(new String("key1"), new String("value1"));
-			dataStructureInstance.insert(new String("key"), new String("value"));
+			dataStructureInstance.insert(new String("key"), new String("value")); //insert one key value pair
+			dataStructureInstance.insert(new String("key1"), new String("value1")); //insert another key value pair
+			dataStructureInstance.insert(new String("key"), new String("value")); //attempt to insert a duplicate key value pair
 			fail("inserted a duplicate key value and no runtime exception was thrown as expected");
-		} catch(RuntimeException exception) {}
+		} catch(RuntimeException exception) {} //see if an exception is thrown
 		
 	}
 	
-	// test04_remove_returns_false_when_key_not_present
 	//insert some key,value pairs, then try removing a key that was not inserted.  Confirm that the return value is false
 	@Test
 	void test04_remove_returns_false_when_key_not_present() {
-		dataStructureInstance.insert(new String("key1"), new String("value1"));
-		dataStructureInstance.insert(new String("key2"), new String("value2"));
-		if(dataStructureInstance.remove("key3") == true) {
+		dataStructureInstance.insert(new String("key1"), new String("value1")); //insert one key value pair
+		dataStructureInstance.insert(new String("key2"), new String("value2")); //insert another key value pair
+		if(dataStructureInstance.remove("key3") == true) { //try to remove a key that was not inserted and see if the test fails
 			fail("attempted to remove a key that was not inserted from data structure and instead of returning false, returned true");
 		}
 	}
@@ -83,44 +88,76 @@ abstract class DataStructureADTTest<T extends DataStructureADT<String,String>> {
 	//insert 50 values, remove 25, get the size
 	@Test
 	void test05_insert_50_remove_25_size_25() {
-		for(int i = 0; i < 50; i++) {
+		for(int i = 0; i < 50; i++) { //insert 50 key value pairs
 			dataStructureInstance.insert(Integer.toString(i), Integer.toString(i));
 		}
-		for(int i = 10; i < 35; i++) {
+		for(int i = 10; i < 35; i++) { //remove 25 of the middle key value pairs
 			dataStructureInstance.remove(Integer.toString(i));
 		}
-		//if(dataStructureInstance.contains("10") == false) fail("fails");
-		if(dataStructureInstance.size() != 25) fail("fails");
+		if(dataStructureInstance.size() != 25) fail("inserted 50 items and removed 25 but the size was not equal to 25"); //check to see if the size reflects the amount inserted and then removed
 	}
 	
+	//attempt to insert a null key and check to see if an exception is thrown
 	@Test 
 	void test06_null_key_exception_is_thrown() {
 		try {
-			dataStructureInstance.insert(null, new String("value"));
-		} catch(IllegalArgumentException exception) {}
+			dataStructureInstance.insert(null, new String("value")); //attempt to insert a new key value pair with a null key
+			fail("attempted to insert a null key into the list but no exception was thrown as it should have been");
+		} catch(IllegalArgumentException exception) {} //check if an exception is thrown
 	}
 	
+	//attempt to remove a null key and check to see if an exception is thrown
 	@Test 
 	void test07_attempt_to_remove_null_key_exception_is_thrown() {
 		try {
-			dataStructureInstance.remove(null);
+			dataStructureInstance.remove(null); //attempt to remove a null value
 			fail("no null key excpetion is thrown when attempting to remove a null key");
-		} catch(IllegalArgumentException exception) {}
+		} catch(IllegalArgumentException exception) {} //check to see if an exception is thrown
 	}
 	
+	//insert a number of items and then call the get method to see if it returns the correct value associated with a given key 
 	@Test 
 	void test08_get_returns_correct_value() {
-		dataStructureInstance.insert(new String("key"), new String("value"));
+		dataStructureInstance.insert(new String("key"), new String("value")); //insert a few key value pairs
 		dataStructureInstance.insert(new String("key1"), new String("value1"));
 		dataStructureInstance.insert(new String("key2"), new String("value2"));
 		dataStructureInstance.insert(new String("key3"), new String("value3"));
 		dataStructureInstance.insert(new String("key4"), new String("value4"));
-		if(dataStructureInstance.get("key3").compareTo("value3") != 0)
-			fail("get method was excepted to return one value but didn't get the correct one");
+		if(dataStructureInstance.get("key3").compareTo("value3") != 0) //get the correct value associated with the key value pair
+			fail("get method was excepted to take a key and return the associated value but didn't get the correct one");
 	}
-	// TODO: add tests to ensure that you can detect implementation that fail
 	
-	// Tip: consider different numbers of inserts and removes and how different combinations of insert and removes
-
+	//call the contains method on a list to check and see if it recognizes when a list has a certain key
+	@Test
+	void test09_check_if_a_list_contains_a_value_that_was_inserted() {
+		for(int i = 0; i < 50; i++) { //insert 50 key value pairs
+			dataStructureInstance.insert(Integer.toString(i), Integer.toString(i));
+		}
+		if(dataStructureInstance.contains("5") == false) fail("expected the key to be found within the list but it was not"); //check to see if list contains one of the inserted key value pairs 
+	}
+	
+	//insert 500 items and remove 500 items to check if list is capable of this quantity and ensure they are all removed
+	@Test
+	void test10_insert_500_remove_500_size_0() {
+		for(int i = 0; i < 500; i++) { //insert 500 key value pairs
+			dataStructureInstance.insert(Integer.toString(i), Integer.toString(i));
+		}
+		for(int i = 0; i < 500; i++) { //remove 500 key value pairs
+			dataStructureInstance.remove(Integer.toString(i));
+		}
+		if(dataStructureInstance.size() != 0) fail("inserted 500 items and removed 500 but the size was not equal to 0");
+	}
+	
+	//check to see if a key can be re-added after being removed from a list and not be counted as a duplicate
+	@Test
+	void test_11_insert_key_then_remove_same_key_and_attempt_to_insert_same_key_again() {
+		try {
+			dataStructureInstance.insert(new String("key"), new String("value")); //insert a key value pair
+			dataStructureInstance.remove(new String("key")); //remove the key value pair
+			dataStructureInstance.insert(new String("key"), new String("value")); //attempt to insert the same key value pair
+		} catch(RuntimeException exception) {
+			fail("attempted to insert a key and value pair after removing the same key and value pair from a list and falsely marked it as a duplicate");
+			}
+	}
 
 }
